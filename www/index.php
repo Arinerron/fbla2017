17fb547ae39613cc9f2064fd06f84b954f6e04b8
Arinerron <arinesau@gmail.com>
2017-12-20 17:33:53 -0800

    fixed

--- www/index.php
+++ www/index.php
@@ -4,12 +4,16 @@
     </head>
     <body id="page-top" data-spy="scroll" data-target=".navbar-custom">
         <div class="bgimg-1">
-            <div class="caption" id="main">
-                <h3 class="main" style="text-align:center;" style="color: #000 !important;">Welcome</h3>
-                <p>sample text</p>
-                <br>
-                <div class="search"><input type="text" placeholder="Enter in a job title..."><input type="submit" value="->"></div>
+
+            <div class="caption" id="main" style="background: none; padding: 0;">
+                <div style="padding-right: 2%; padding-left: 2%; padding-bottom: 2%; background-color: rgba(255, 255, 255, 0.5); padding-top: 0.25%; width: 300px; margin: 0 auto;">
+                    <h3 class="main" style="text-align:center;" style="color: #000 !important;">Welcome</h3>
+                    <p>sample text</p>
+                    <br>
+                    <div class="search"><input type="text" placeholder="Enter in a job title..."><input type="submit" class="submit" value="->"></div>
+                </div>
             </div>
+
         </div>
         <div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
             <h3 style="text-align:center;">test</h3>
