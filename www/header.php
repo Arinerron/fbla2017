4ece43d223ea6ee0a8d6e6d6edbcd193c56c4ab2
Arinerron <arinesau@gmail.com>
2017-12-20 17:02:45 -0800

    add styling

--- www/header.php
+++ www/header.php
@@ -20,7 +20,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a href="/" class="navbar-brand">openings.guide</a>
+          <a href="/" class="navbar-brand">insert site name later</a>
         </div>
 
         <div class="collapse navbar-collapse" id="navbar-collapse-1">
